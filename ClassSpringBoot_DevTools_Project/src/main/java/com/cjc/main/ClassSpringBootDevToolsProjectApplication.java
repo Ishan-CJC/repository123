b7d6c13f3655c71b80fc3484ec30e689fc97a385
ishan <ishan@ishan-PC>
2021-02-10 15:37:01 +0530

    ish commit

---
+++ ClassSpringBoot_DevTools_Project/src/main/java/com/cjc/main/ClassSpringBootDevToolsProjectApplication.java
@@ -0,0 +1,14 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassSpringBootDevToolsProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Dev Tool");
+		SpringApplication.run(ClassSpringBootDevToolsProjectApplication.class, args);
+	}
+
+}
